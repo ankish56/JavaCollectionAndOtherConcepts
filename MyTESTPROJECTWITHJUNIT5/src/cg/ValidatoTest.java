8dc64a650aa0ad54d7bcd05af0d792fde6452729
ankish56 <51943290+ankish56@users.noreply.github.com>
2019-06-24 17:37:37 +0530

    Update ValidatoTest.java

--- MyTESTPROJECTWITHJUNIT5/src/cg/ValidatoTest.java
+++ MyTESTPROJECTWITHJUNIT5/src/cg/ValidatoTest.java
@@ -1,12 +1,59 @@
-package cg;
-
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class ValidatoTest {
 
+	@ParameterizedTest
+	@ValueSource(strings= {"racecar","radar","nitin","naman"})
+	void test3(String data) {
+		Validator ob = new Validator();
+		System.out.println("test ispalindrome with value" + data);
+		assertTrue(ob.isPalindrome(data));
+	}
+	
+	
+	@ParameterizedTest
+	@CsvSource({"4,5","10,20","100,500"})
+	void test4(int a,int b) {
+		Calculator ob = new Calculator();
+		System.out.println("test add with values a=" +a+",b="+b);
+		assertTrue(ob.add(a, b)>0);
+	}
+	
+	@ParameterizedTest
+	@MethodSource("testaddwithoutput")
+	void withMethodSource(int a,int b ,int op) {
+		Calculator ob = new Calculator();
+		System.out.println("test add with values a=" +a+",b="+b+"output"+op);
+		assertEquals(op,ob.add(a, b));
+	}
+	
+	
+	private static Stream<Arguments>testaddwithoutput(){
+		return Stream.of(
+				Arguments.of(4,5,9),
+				Arguments.of(100,500,600));
+				}
+	
+	@ParameterizedTest
+	@EnumSource(Size.class)
+	void withAllEnumValues(Size size) {
+		System.out.println("Test Enum Values" + size);
+		Validator ob = new Validator();
+		assertEquals(size.getMl(),ob.getSizeinMl(size));
+	}
+	
+	
 	@Test
 	void test1() {
 		Validator ob = new Validator();
